4826eeadfd729d3d9cd6c1fe1a60ea55f3c989ea
anuyashkoli <anuyashkoli@gmail.com>
2025-12-21 16:31:03 +0530

    Implemented CRUD Methods

--- app/src/main/java/com/app/timetable/data/local/dao/SubjectDao.kt
+++ app/src/main/java/com/app/timetable/data/local/dao/SubjectDao.kt
@@ -1,4 +1,21 @@
 package com.app.timetable.data.local.dao
 
-class SubjectDao {
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import com.app.timetable.data.local.entity.Subject
+import kotlinx.coroutines.flow.Flow
+
+interface SubjectDao {
+    @Upsert
+    suspend fun upsertSubject(subject: Subject)
+
+    @Delete
+    suspend fun deleteSubject(subject: Subject)
+
+    @Query("SELECT * FROM subjectTable ORDER BY priority ASC")
+    fun getAllSubjects(): Flow<List<Subject>>
+
+    @Query("SELECT * FROM subjectTable WHERE subjectID = 'subjectID'")
+    fun getSubjectByID(subjectID: Int): Flow<List<Subject>>
 }
