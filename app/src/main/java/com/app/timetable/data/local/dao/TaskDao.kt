d8e467418be5a4624a1481cadd4b3f98a465cd9b
anuyashkoli <anuyashkoli@gmail.com>
2025-12-23 16:56:58 +0530

    Added Upsert, Delete Method and getAllTasks() function

--- app/src/main/java/com/app/timetable/data/local/dao/TaskDao.kt
+++ app/src/main/java/com/app/timetable/data/local/dao/TaskDao.kt
@@ -1,13 +1,24 @@
 package com.app.timetable.data.local.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Query
+import androidx.room.Upsert
 import com.app.timetable.data.local.entity.Task
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 
 interface TaskDao {
+    @Upsert
+    suspend fun upsertTask(task: Task)
+
+    @Delete
+    suspend fun deleteTask(task: Task)
+
+    @Query("SELECT * FROM taskTable ORDER BY priority ASC")
+    fun getAllTasks(): Flow<List<Task>>
+
     @Query("SELECT * FROM taskTable WHERE isCompleted = 0 ORDER BY priority ASC ") // Ascending; 1 is High and 5 is Low
     fun dueHighPriorityTasks(): Flow<List<Task>>
 
