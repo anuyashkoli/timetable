c8df2e0312931017026d569c3e945448f9b345da
anuyashkoli <anuyashkoli@gmail.com>
2025-12-23 18:47:27 +0530

    feat: Add material icons extended dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,4 +65,5 @@ dependencies {
     implementation(libs.hilt.android)
     ksp(libs.hilt.compiler)
     implementation(libs.androidx.hilt.navigation.compose)
+    implementation(libs.androidx.compose.material.icons.extended)
 }
