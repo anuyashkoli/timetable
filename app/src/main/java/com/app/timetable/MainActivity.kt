0194399beb91a016c36faf02dae91c4e5cac8019
anuyashkoli <anuyashkoli@gmail.com>
2025-12-23 18:45:44 +0530

    Integrated Hilt and set up HomeScreen

--- app/src/main/java/com/app/timetable/MainActivity.kt
+++ app/src/main/java/com/app/timetable/MainActivity.kt
@@ -12,36 +12,38 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.app.timetable.ui.theme.TimetableTheme
+import dagger.hilt.android.AndroidEntryPoint
+import com.app.timetable.ui.screens.HomeScreen
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             TimetableTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                HomeScreen(
+                    onAddTaskClick = {
+                        // TODO: Navigate to AddTaskScreen
+                    }
+                )
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    @Composable
+    fun Greeting(name: String, modifier: Modifier = Modifier) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TimetableTheme {
-        Greeting("Android")
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        TimetableTheme {
+            Greeting("Android")
+        }
     }
 }
